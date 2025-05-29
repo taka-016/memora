9707a6bd75f5d3d016d9bbc2d4cad727e6948bd4
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-05-29 22:57:22 +0900

    マップ表示追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,8 +6,8 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:flutter_verification/main.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   testWidgets('トップメニューが表示される', (WidgetTester tester) async {
@@ -16,5 +16,20 @@ void main() {
     expect(find.text('ダミー機能A'), findsOneWidget);
     expect(find.text('ダミー機能B'), findsOneWidget);
     expect(find.text('トップメニュー'), findsOneWidget);
+    expect(find.text('マップ表示'), findsOneWidget);
+  });
+
+  testWidgets('マップ表示メニューをタップするとGoogleMap画面が表示される', (WidgetTester tester) async {
+    await tester.pumpWidget(const MyApp());
+
+    // 「マップ表示」メニューをタップ
+    await tester.tap(find.text('マップ表示'));
+    await tester.pumpAndSettle();
+
+    // AppBarタイトル確認
+    expect(find.text('Googleマップ'), findsOneWidget);
+
+    // GoogleMapウィジェットが存在することを確認
+    expect(find.byType(GoogleMap), findsOneWidget);
   });
 }
