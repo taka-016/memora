bc87e233428f86b43568dde76795486fdcc10743
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-05-30 22:25:55 +0900

    リファクタリング

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:flutter/services.dart';
+import 'presentation/home_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -9,182 +8,17 @@ void main() {
   runApp(const MyApp());
 }
 
-class MapScreen extends StatefulWidget {
-  const MapScreen({super.key});
-
-  @override
-  State<MapScreen> createState() => _MapScreenState();
-}
-
-class _MapScreenState extends State<MapScreen> {
-  static const LatLng _initialPosition = LatLng(35.681236, 139.767125); // 東京駅
-  final Set<Marker> _markers = {};
-  GoogleMapController? _mapController;
-
-  @override
-  void initState() {
-    super.initState();
-    // 初期ピンはなし
-  }
-
-  void _onMapCreated(GoogleMapController controller) {
-    _mapController = controller;
-  }
-
-  void _addPin(LatLng position) {
-    final markerId = MarkerId(position.toString());
-    setState(() {
-      _markers.add(
-        Marker(
-          markerId: markerId,
-          position: position,
-          infoWindow: const InfoWindow(title: 'ピン'),
-          onTap: () => _removePin(markerId),
-        ),
-      );
-    });
-  }
-
-  void _removePin(MarkerId markerId) {
-    setState(() {
-      _markers.removeWhere((m) => m.markerId == markerId);
-    });
-  }
-
-  Future<void> _moveToCurrentLocation() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      await Geolocator.openLocationSettings();
-      return;
-    }
-    LocationPermission permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        return;
-      }
-    }
-    if (permission == LocationPermission.deniedForever) {
-      return;
-    }
-    final position = await Geolocator.getCurrentPosition();
-    final latLng = LatLng(position.latitude, position.longitude);
-    _mapController?.animateCamera(CameraUpdate.newLatLng(latLng));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Googleマップ')),
-      body: Stack(
-        children: [
-          GoogleMap(
-            onMapCreated: _onMapCreated,
-            initialCameraPosition: const CameraPosition(
-              target: _initialPosition,
-              zoom: 15,
-            ),
-            markers: _markers,
-            onTap: _addPin,
-            myLocationEnabled: true,
-            myLocationButtonEnabled: false,
-          ),
-          // 拡大縮小ボタンと重ならないように下部余白を広げる
-          Positioned(
-            bottom: 100,
-            right: 4,
-            child: FloatingActionButton(
-              onPressed: _moveToCurrentLocation,
-              child: const Icon(Icons.my_location),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    final menuItems = [
-      {
-        'title': 'マップ表示',
-        'onTap': () {
-          Navigator.of(
-            context,
-          ).push(MaterialPageRoute(builder: (_) => const MapScreen()));
-        },
-      },
-      {'title': 'ダミー機能A', 'onTap': () {}},
-      {'title': 'ダミー機能B', 'onTap': () {}},
-    ];
-
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('トップメニュー'),
-      ),
-      body: ListView.builder(
-        itemCount: menuItems.length,
-        itemBuilder: (context, index) {
-          final item = menuItems[index];
-          return ListTile(
-            title: Text(item['title'] as String),
-            trailing: const Icon(Icons.arrow_forward_ios),
-            onTap: item['onTap'] as void Function(),
-          );
-        },
-      ),
     );
   }
 }
---
+++ lib/presentation/home_page.dart
@@ -0,0 +1,48 @@
+/// トップメニュー画面
+import 'package:flutter/material.dart';
+import 'map_screen.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final menuItems = [
+      {
+        'title': 'マップ表示',
+        'onTap': () {
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (_) => const MapScreen()));
+        },
+      },
+      {'title': 'ダミー機能A', 'onTap': () {}},
+      {'title': 'ダミー機能B', 'onTap': () {}},
+    ];
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('トップメニュー'),
+      ),
+      body: ListView.builder(
+        itemCount: menuItems.length,
+        itemBuilder: (context, index) {
+          final item = menuItems[index];
+          return ListTile(
+            title: Text(item['title'] as String),
+            trailing: const Icon(Icons.arrow_forward_ios),
+            onTap: item['onTap'] as void Function(),
+          );
+        },
+      ),
+    );
+  }
+}
