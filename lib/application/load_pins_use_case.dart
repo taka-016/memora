bb46c477a5127fc0bccb7e6a7c473f91307367e9
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-05-31 23:52:21 +0900

    LoadPinsUseCase追加

---
+++ lib/application/load_pins_use_case.dart
@@ -0,0 +1,14 @@
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import '../infrastructure/pin_repository.dart';
+
+class LoadPinsUseCase {
+  final PinRepository _pinRepository;
+
+  LoadPinsUseCase(this._pinRepository);
+
+  /// ピンの位置リストを取得し、Google Maps用のLatLngリストに変換して返す
+  Future<List<LatLng>> execute() async {
+    final pins = await _pinRepository.getPins();
+    return pins.map((pin) => LatLng(pin.latitude, pin.longitude)).toList();
+  }
+}
