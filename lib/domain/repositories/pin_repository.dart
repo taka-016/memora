58ada1b935a418d6a33ee7169822af2263eb1ffb
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-06-02 21:31:52 +0900

    Firebaseから該当ピンを削除する処理

--- lib/domain/repositories/pin_repository.dart
+++ lib/domain/repositories/pin_repository.dart
@@ -4,4 +4,5 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 abstract class PinRepository {
   Future<List<Pin>> getPins();
   Future<void> savePin(LatLng position);
+  Future<void> deletePin(String pinId);
 }
