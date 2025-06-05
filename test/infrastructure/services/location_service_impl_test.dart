221fd9d5daf7e2f3d3642ab297288146c7f16e94
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-06-05 20:59:01 +0900

    LocationServiceImpl→GeolocatorLocationService

--- test/infrastructure/services/location_service_impl_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_verification/infrastructure/services/location_service_impl.dart';
-
-void main() {
-  test('LocationServiceImplが生成できる', () {
-    final service = LocationServiceImpl();
-    expect(service, isA<LocationServiceImpl>());
-  });
-}
