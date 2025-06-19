ec653697f1f6bbafbb46e1e64dc8e987a79941ba
otokawa-k <kiyotaka.otokawa@tatsu-mi.co.jp>
2025-06-19 12:35:11 +0000

    accountを削除

--- lib/domain/repositories/account_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:memora/domain/entities/account.dart';
-
-abstract class AccountRepository {
-  Future<List<Account>> getAccounts();
-  Future<void> saveAccount(Account account);
-  Future<void> deleteAccount(String accountId);
-  Future<Account?> getAccountById(String accountId);
-  Future<Account?> getAccountByEmail(String email);
-}
