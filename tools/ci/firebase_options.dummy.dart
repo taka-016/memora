b4ee0216cb1c5563f47a97ab594ffeab26d7d98e
taka-016 <k.otokawa.work@gmail.com>
2025-08-03 22:47:39 +0900

    ワークフロー修正
    Actions用のDockerfile作成
    
    テスト用のダミーファイル作成
    
    ワークフロー修正
    
    ダミーファイル修正
    
    ワークフロー修正
    
    ワークフロー修正
    
    ワークフロー修正
    
    デバッグ
    
    ワークフロー修正
    
    Dockerfile更新

---
+++ tools/ci/firebase_options.dummy.dart
@@ -0,0 +1,24 @@
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kIsWeb, TargetPlatform;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    if (kIsWeb) {
+      throw UnsupportedError('Web not configured');
+    }
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      default:
+        throw UnsupportedError('Platform not supported');
+    }
+  }
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'dummy-api-key',
+    appId: 'dummy-app-id',
+    messagingSenderId: 'dummy-sender-id',
+    projectId: 'dummy-project-id',
+  );
+}
