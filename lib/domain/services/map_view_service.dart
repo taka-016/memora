523c8135adbf314cd9b23af3d830b781ed416344
taka-016 <k.otokawa.work@gmail.com>
2025-08-16 23:07:43 +0900

    コメント削除

--- lib/domain/services/map_view_service.dart
+++ lib/domain/services/map_view_service.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:memora/domain/value-objects/location.dart';
 import 'package:memora/domain/entities/pin.dart';
 
-/// 地図ビュー表示のためのサービスインターフェース
 abstract class MapViewService {
-  /// 地図ビューウィジェットを作成する
   Widget createMapView({
     required List<Pin> pins,
     Function(Location)? onMapLongTapped,
