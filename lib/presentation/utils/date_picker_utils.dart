7207fb0e9827f619045b258c9b64555c11bdbd9d
taka-016 <k.otokawa.work@gmail.com>
2025-08-22 22:11:41 +0900

    日付と時間は別々で入力する

--- lib/presentation/utils/date_picker_utils.dart
+++ lib/presentation/utils/date_picker_utils.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import '../widgets/custom_date_picker_dialog.dart';
-import '../widgets/custom_datetime_picker_dialog.dart';
 
 /// DatePickerに関するユーティリティクラス
 class DatePickerUtils {
@@ -16,23 +15,6 @@ class DatePickerUtils {
     return await showCustomDatePickerDialog(
       context,
       initialDate: initialDate,
-      firstDate: firstDate,
-      lastDate: lastDate,
-    );
-  }
-
-  /// カスタマイズされたDateTimePickerを表示する
-  ///
-  /// 日時（時分まで）の入力が可能
-  static Future<DateTime?> showCustomDateTimePicker(
-    BuildContext context, {
-    required DateTime initialDateTime,
-    required DateTime firstDate,
-    required DateTime lastDate,
-  }) async {
-    return await showCustomDateTimePickerDialog(
-      context,
-      initialDateTime: initialDateTime,
       firstDate: firstDate,
       lastDate: lastDate,
     );
