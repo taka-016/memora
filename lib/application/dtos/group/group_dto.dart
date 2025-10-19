08cc05e5364e1be15ddd28135f52c394210209da
taka-016 <k.otokawa.work@gmail.com>
2025-10-19 21:49:34 +0900

    refactor(dto): GroupDtoをEquatable化してcopyWithを追加
    GroupMemberDtoと同様に、GroupDtoをEquatableに準拠させ、
    copyWithメソッドを実装することで、不変性を保ちながら
    プロパティを更新できるようにした。
    
    - Equatableを継承してequality比較を可能にした
    - copyWithメソッドを実装して部分的な更新をサポート
    - constコンストラクタを使用してパフォーマンスを向上
    - propsに全フィールドを定義してequality比較を実装

--- lib/application/dtos/group/group_dto.dart
+++ lib/application/dtos/group/group_dto.dart
@@ -1,17 +1,37 @@
+import 'package:equatable/equatable.dart';
 import 'package:memora/application/dtos/group/group_member_dto.dart';
 
-class GroupDto {
-  final String id;
-  final String ownerId;
-  final String name;
-  final String? memo;
-  final List<GroupMemberDto> members;
-
-  GroupDto({
+class GroupDto extends Equatable {
+  const GroupDto({
     required this.id,
     required this.ownerId,
     required this.name,
     this.memo,
     required this.members,
   });
+
+  final String id;
+  final String ownerId;
+  final String name;
+  final String? memo;
+  final List<GroupMemberDto> members;
+
+  GroupDto copyWith({
+    String? id,
+    String? ownerId,
+    String? name,
+    String? memo,
+    List<GroupMemberDto>? members,
+  }) {
+    return GroupDto(
+      id: id ?? this.id,
+      ownerId: ownerId ?? this.ownerId,
+      name: name ?? this.name,
+      memo: memo ?? this.memo,
+      members: members ?? this.members,
+    );
+  }
+
+  @override
+  List<Object?> get props => [id, ownerId, name, memo, members];
 }
