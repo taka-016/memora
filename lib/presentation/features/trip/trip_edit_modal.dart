69fa1c1557d054a5712334d2dd31ac002d421ce8
taka-016 <k.otokawa.work@gmail.com>
2026-04-03 15:47:52 +0900

    refactor(uuid): タスクとピンを標準表記へ戻す

--- lib/presentation/features/trip/trip_edit_modal.dart
+++ lib/presentation/features/trip/trip_edit_modal.dart
@@ -125,7 +125,7 @@ class TripEditModal extends HookConsumerWidget {
 
     void addPin({required Coordinate coordinate, String? locationName}) {
       final newPin = PinDto(
-        pinId: const Uuid().v7().replaceAll('-', ''),
+        pinId: const Uuid().v7(),
         latitude: coordinate.latitude,
         longitude: coordinate.longitude,
         locationName: locationName,
