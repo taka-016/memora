d7e6afdab0a9eeb467f55973daf5dc6204d5281e
taka-016 <k.otokawa.work@gmail.com>
2026-04-19 22:49:16 +0900

    refactor(ui): GroupSelectionListをHookWidget化

--- lib/presentation/shared/group_selection/group_selection_list.dart
+++ lib/presentation/shared/group_selection/group_selection_list.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:memora/application/dtos/group/group_dto.dart';
 import 'package:memora/core/app_logger.dart';
 
 enum GroupSelectionListState { loading, groupList, empty, error }
 
-class GroupSelectionList extends HookConsumerWidget {
+class GroupSelectionList extends HookWidget {
   final void Function(GroupDto)? onGroupSelected;
   final String title;
   final Key listKey;
@@ -23,7 +22,7 @@ class GroupSelectionList extends HookConsumerWidget {
   });
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     final state = useState(GroupSelectionListState.loading);
     final groupsWithMembers = useState<List<GroupDto>>(<GroupDto>[]);
     final errorMessage = useState('');
