406d47d29ea0653b06d07dc027e90b863bd3b1e0
taka-016 <k.otokawa.work@gmail.com>
2026-05-24 21:51:04 +0900

    test(android-widget): 矢印操作の失敗処理を検証

---
+++ test/unit/application/usecases/android_widget/android_widget_itinerary_cache_usecases_test.dart
@@ -0,0 +1,184 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:memora/application/dtos/android_widget/android_widget_itinerary_cache_dto.dart';
+import 'package:memora/application/dtos/trip/itinerary_item_dto.dart';
+import 'package:memora/application/dtos/trip/trip_entry_dto.dart';
+import 'package:memora/application/queries/order_by.dart';
+import 'package:memora/application/queries/trip/itinerary_item_query_service.dart';
+import 'package:memora/application/queries/trip/trip_entry_query_service.dart';
+import 'package:memora/application/services/android_widget_cache_storage.dart';
+import 'package:memora/application/usecases/android_widget/android_widget_itinerary_cache_usecases.dart';
+import 'package:memora/application/usecases/android_widget/get_android_widget_itinerary_cache_usecase.dart';
+import 'package:memora/core/time/app_clock.dart';
+
+import '../../../../helpers/test_exception.dart';
+
+void main() {
+  group('MoveAndroidWidgetSelectedItineraryDateUsecase', () {
+    test('リモート探索で失敗した場合はエラーを保存してウィジェットを更新する', () async {
+      final storage = _FakeAndroidWidgetCacheStorage(
+        cache: AndroidWidgetItineraryCacheDto(
+          version: 1,
+          groupId: 'group-1',
+          selectedItineraryDateId: 'trip-1_2026-05-24',
+          lastUpdatedAt: DateTime(2026, 5, 24, 10),
+          itineraryDates: [
+            AndroidWidgetItineraryDateCacheDto(
+              id: 'trip-1_2026-05-24',
+              tripId: 'trip-1',
+              tripName: '旅行',
+              tripPeriodLabel: '2026/5/24 - 2026/5/25',
+              date: DateTime(2026, 5, 24),
+              dateLabel: '2026/5/24',
+              itineraryItems: [],
+            ),
+          ],
+        ),
+      );
+      final tripEntryQueryService = _FakeTripEntryQueryService()
+        ..exception = TestException('取得失敗');
+      final itineraryItemQueryService = _FakeItineraryItemQueryService();
+      final usecase = MoveAndroidWidgetSelectedItineraryDateUsecase(
+        cacheStorage: storage,
+        tripEntryQueryService: tripEntryQueryService,
+        itineraryItemQueryService: itineraryItemQueryService,
+        refreshCacheUsecase: _buildRefreshUsecase(
+          storage,
+          tripEntryQueryService,
+          itineraryItemQueryService,
+        ),
+      );
+
+      await expectLater(
+        usecase.execute(AndroidWidgetItineraryDateMoveDirection.next),
+        completes,
+      );
+
+      expect(storage.errorMessage, '切り替えに失敗しました');
+      expect(storage.updateWidgetCount, 1);
+    });
+  });
+}
+
+RefreshAndroidWidgetItineraryCacheUsecase _buildRefreshUsecase(
+  AndroidWidgetCacheStorage storage,
+  TripEntryQueryService tripEntryQueryService,
+  ItineraryItemQueryService itineraryItemQueryService,
+) {
+  return RefreshAndroidWidgetItineraryCacheUsecase(
+    cacheStorage: storage,
+    getCacheUsecase: GetAndroidWidgetItineraryCacheUsecase(
+      tripEntryQueryService: tripEntryQueryService,
+      itineraryItemQueryService: itineraryItemQueryService,
+      clock: FixedAppClock(DateTime(2026, 5, 24, 10)),
+    ),
+  );
+}
+
+class _FakeAndroidWidgetCacheStorage implements AndroidWidgetCacheStorage {
+  _FakeAndroidWidgetCacheStorage({this.cache});
+
+  AndroidWidgetItineraryCacheDto? cache;
+  String? targetGroupId;
+  String? selectedItineraryDateId;
+  String? errorMessage;
+  int updateWidgetCount = 0;
+
+  @override
+  Future<void> clear() async {
+    cache = null;
+    targetGroupId = null;
+    selectedItineraryDateId = null;
+    errorMessage = null;
+  }
+
+  @override
+  Future<void> clearTargetGroupId() async {
+    targetGroupId = null;
+  }
+
+  @override
+  Future<String?> getSelectedItineraryDateId() async {
+    return selectedItineraryDateId;
+  }
+
+  @override
+  Future<String?> getTargetGroupId() async {
+    return targetGroupId;
+  }
+
+  @override
+  Future<AndroidWidgetItineraryCacheDto?> loadItineraryCache() async {
+    return cache;
+  }
+
+  @override
+  Future<void> saveErrorMessage(String? message) async {
+    errorMessage = message;
+  }
+
+  @override
+  Future<void> saveItineraryCache(AndroidWidgetItineraryCacheDto cache) async {
+    this.cache = cache;
+    selectedItineraryDateId = cache.selectedItineraryDateId;
+  }
+
+  @override
+  Future<void> saveSelectedItineraryDateId(String? itineraryDateId) async {
+    selectedItineraryDateId = itineraryDateId;
+  }
+
+  @override
+  Future<void> saveTargetGroupId(String groupId) async {
+    targetGroupId = groupId;
+  }
+
+  @override
+  Future<void> updateWidget() async {
+    updateWidgetCount += 1;
+  }
+}
+
+class _FakeTripEntryQueryService implements TripEntryQueryService {
+  Object? exception;
+
+  @override
+  Future<TripEntryDto?> getTripEntryById(
+    String tripId, {
+    List<OrderBy>? pinsOrderBy,
+    List<OrderBy>? tasksOrderBy,
+    List<OrderBy>? itineraryItemsOrderBy,
+  }) async {
+    return null;
+  }
+
+  @override
+  Future<List<TripEntryDto>> getTripEntriesByGroupId(
+    String groupId, {
+    List<OrderBy>? orderBy,
+  }) async {
+    final exception = this.exception;
+    if (exception != null) {
+      throw exception;
+    }
+    return [];
+  }
+
+  @override
+  Future<List<TripEntryDto>> getTripEntriesByGroupIdAndYear(
+    String groupId,
+    int year, {
+    List<OrderBy>? orderBy,
+  }) async {
+    return [];
+  }
+}
+
+class _FakeItineraryItemQueryService implements ItineraryItemQueryService {
+  @override
+  Future<List<ItineraryItemDto>> getItineraryItemsByTripId(
+    String tripId, {
+    List<OrderBy>? orderBy,
+  }) async {
+    return [];
+  }
+}
