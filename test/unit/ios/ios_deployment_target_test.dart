7dfc37187ba65647cb06c3c4c34ef30f66abe88b
taka-016 <k.otokawa.work@gmail.com>
2026-06-22 19:22:37 +0900

    test(ios): deployment targetの回帰テストを追加

---
+++ test/unit/ios/ios_deployment_target_test.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter_test/flutter_test.dart';
+
+const _xcodeProjectPath = 'ios/Runner.xcodeproj/project.pbxproj';
+
+void main() {
+  group('iOS Deployment Target', () {
+    test('全ビルド構成でworkmanagerが要求するiOS 14.0以上を指定する', () {
+      final project = File(_xcodeProjectPath).readAsStringSync();
+      final deploymentTargets = RegExp(
+        r'IPHONEOS_DEPLOYMENT_TARGET = ([\d.]+);',
+      ).allMatches(project);
+
+      expect(deploymentTargets, hasLength(3));
+      expect(
+        deploymentTargets.map((match) => match.group(1)),
+        everyElement('14.0'),
+      );
+    });
+  });
+}
