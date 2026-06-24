22465ef5346780e450ab93e1cfcef2e40134eadb
taka-016 <k.otokawa.work@gmail.com>
2026-06-24 14:42:38 +0900

    refactor(test): ウィジェット設定の読み込みを共有

--- test/unit/android/android_widget_size_test.dart
+++ test/unit/android/android_widget_size_test.dart
@@ -6,15 +6,13 @@ const _widgetInfoPath =
     'android/app/src/main/res/xml/itinerary_widget_info.xml';
 
 void main() {
-  test('Androidウィジェットの初期幅を4セルにする', () {
-    final source = File(_widgetInfoPath).readAsStringSync();
+  final source = File(_widgetInfoPath).readAsStringSync();
 
+  test('Androidウィジェットの初期幅を4セルにする', () {
     expect(source, contains('android:targetCellWidth="4"'));
   });
 
   test('Androidウィジェットの初期高さを3セルにする', () {
-    final source = File(_widgetInfoPath).readAsStringSync();
-
     expect(source, contains('android:targetCellHeight="3"'));
     expect(source, contains('android:minHeight="180dp"'));
   });
