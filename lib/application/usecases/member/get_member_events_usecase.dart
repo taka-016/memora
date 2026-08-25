1bfc109646e0847208c09ef870d1f2d47db25177
taka-016 <k.otokawa.work@gmail.com>
2026-08-25 22:48:50 +0900

    refactor(timeline): イベント取得例外を伝播

--- lib/application/usecases/member/get_member_events_usecase.dart
+++ lib/application/usecases/member/get_member_events_usecase.dart
@@ -2,7 +2,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:memora/application/dtos/member/member_event_dto.dart';
 import 'package:memora/application/queries/member/member_event_query_service.dart';
 import 'package:memora/application/queries/order_by.dart';
-import 'package:memora/core/app_logger.dart';
 import 'package:memora/infrastructure/factories/query_service_factory.dart';
 
 final getMemberEventsUsecaseProvider = Provider<GetMemberEventsUsecase>((ref) {
@@ -15,18 +14,9 @@ class GetMemberEventsUsecase {
   GetMemberEventsUsecase(this._memberEventQueryService);
 
   Future<List<MemberEventDto>> execute(List<String> memberIds) async {
-    try {
-      return await _memberEventQueryService.getMemberEventsByMemberIds(
-        memberIds,
-        orderBy: const [OrderBy('year', descending: false)],
-      );
-    } catch (e, stack) {
-      logger.e(
-        'GetMemberEventsUsecase.execute: ${e.toString()}',
-        error: e,
-        stackTrace: stack,
-      );
-      return [];
-    }
+    return await _memberEventQueryService.getMemberEventsByMemberIds(
+      memberIds,
+      orderBy: const [OrderBy('year', descending: false)],
+    );
   }
 }
